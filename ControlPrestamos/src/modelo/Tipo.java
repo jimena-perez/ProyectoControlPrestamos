e047e4ee82c552283c34da91e42932549d612794
jimena-perez <167660412+jimena-perez@users.noreply.github.com>
2026-06-11 17:27:35 -0600

    Add files via upload
    agregado clase tipo

--- ControlPrestamos/src/modelo/Tipo.java
+++ ControlPrestamos/src/modelo/Tipo.java
@@ -1,5 +1,41 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Tipo {
 
+    private String nombre;
+    private ArrayList<Item> items;
+
+    public Tipo(String nombre) {
+        this.nombre = nombre;
+        this.items = new ArrayList<Item>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void agregarItem(Item item) {
+        if (item != null && !items.contains(item)) {
+            items.add(item);
+        }
+    }
+
+    public void eliminarItem(Item item) {
+        items.remove(item);
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
